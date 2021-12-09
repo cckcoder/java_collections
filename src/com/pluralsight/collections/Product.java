69d2ae7268ee09fa52f9b6656ed89cbef2482447
chaiwat <chaiwat.chaobaankoh@accenture.com>
2021-12-09 10:29:45 +0700

    Initial commit

---
+++ src/com/pluralsight/collections/Product.java
@@ -0,0 +1,27 @@
+package com.pluralsight.collections;
+
+public class Product {
+    private final String name;
+    private final int weight;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public Product(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "name='" + name + '\'' +
+                ", weight=" + weight +
+                '}';
+    }
+}
---
+++ src/com/pluralsight/collections/TheArrayProblem.java
@@ -0,0 +1,26 @@
+package com.pluralsight.collections;
+
+import java.util.Arrays;
+
+public class TheArrayProblem {
+    public static void main(String[] args) {
+        Product door = new Product("Wooden Door", 35);
+        Product floorPanel = new Product("Floor Panel", 25);
+        Product window = new Product("Glass Window", 10);
+
+        // Create
+        Product[] products = { door, floorPanel };
+        System.out.println(Arrays.toString(products));
+
+        // Add
+        products = add(window, products);
+        System.out.println(Arrays.toString(products));
+    }
+
+    private static Product[] add(Product product, Product[] array){
+        int length = array.length;
+        Product[] newArray = Arrays.copyOf(array, length + 1);
+        newArray[length] = product;
+        return newArray;
+    }
+}
